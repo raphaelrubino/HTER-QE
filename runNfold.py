0c17cc0d006f33e73df0594dbac135d5ef150a26
Rafael <rafa@turing.uni-saarland.de>
2017-01-18 17:47:25 +0100

    Update scripts

--- runNfold.py
+++ runNfold.py
@@ -13,17 +13,17 @@ from sklearn import metrics
 from math import sqrt
 
 
-if len( sys.argv ) != 4:
-	print( "\nUsage: ", sys.argv[0], "<libsvm train (string)> <n fold (int)> <nb threads (int)> \n" )
+if len( sys.argv ) != 5:
+	print( "\nUsage: ", sys.argv[0], "<libsvm train (string)> <n fold (int)> <nb threads (int)> <output prefix>\n" )
 	exit()
 
-feat, nfold, nbthreads = sys.argv[1:]
+feat, nfold, nbthreads, out = sys.argv[1:]
 nfold = int( nfold )
 nbthreads = int( nbthreads )
 
-C = np.logspace( -15.0, 8.0, num = 30, endpoint = True, base = 2 )
-G = np.logspace( -15.0, -0.1, num = 20, endpoint = True, base = 2 )
-E = np.logspace( -6.0, -2.0, num = 10, endpoint = True, base = 2 )
+C = np.logspace( -12.0, 6.0, num = 20, endpoint = True, base = 2 )
+G = np.logspace( -10.0, -0.1, num = 15, endpoint = True, base = 2 )
+E = np.logspace( -8.0, -4.0, num = 8, endpoint = True, base = 2 )
 
 X, y = load_svmlight_file( feat )
 params = [ { 'kernel': [ 'rbf' ], 'gamma': G, 'C': C, 'epsilon': E } ]
@@ -37,4 +37,6 @@ for i in range( nfold ):
 	clf.fit( X_train, y_train )
 	print( clf.best_params_ )
 	y_pred = clf.predict( X_test )
-	print( "MAE", metrics.mean_absolute_error( y_test, y_pred ) )
+	with open( out + ".fold" + str( i ) + ".pred", "w" ) as outfile:
+		outfile.write( "\n".join( [ str( item ) for item in y_pred ] ) )
+	print( "Pearson r", metrics.pearson( y_test, y_pred ) )
